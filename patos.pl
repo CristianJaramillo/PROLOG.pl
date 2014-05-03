2817a02732fa51a49105edefaa5a1984bc761e1f
By Cristian Jaramillo <cristianjaramillo32@gmail.com>
2014-05-02 23:06:04 -0500

    Add files

---
+++ patos.pl
@@ -0,0 +1,9 @@
+esPato(lucas).
+esPato(donald).
+esPato(gilito).
+sobrino(jorgito,donald).
+sobrino(jaimito,donald).
+sobrino(juanito,donald).
+tienePlumas(piolin).
+tienePlumas(daisy).
+haceCuac(daisy).
